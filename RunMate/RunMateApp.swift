6cc25e7f1da9e20c8ba97fe8771c99fe63d985ad
Jörn Otte <joern.otte@gmx.net>
2023-02-19 18:42:46 +0100

    Initial Commit

---
+++ RunMate/RunMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  RunMateApp.swift
+//  RunMate
+//
+//  Created by Jörn Otte on 19.02.23.
+//
+
+import SwiftUI
+
+@main
+struct RunMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
